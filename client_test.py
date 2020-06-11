bd034faf4e8ef33e11458ec9ef0231bff1445aa3
Mariana Chavez <57697974+Mchavez-on@users.noreply.github.com>
2020-06-11 14:10:40 -0700

    client_test_1.py
    In the case of this task, the methods we want to test are getDataPoint and
    getRatio. We want to cover these with tests so that we ensure these
    methods are returning the correct output as individual methods thereby
    ensuring us that when we use them together in the main client application,
    things will work fine.
    In unit testing, we always follow the pattern, Build-Act-Assert. Assertions should mostly be used once and be the strongest.

--- client_test.py
+++ client_test.py
@@ -2,14 +2,16 @@ import unittest
 from client3 import getDataPoint
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
+  def test_getDataPoint_calculatePrice(self): #test1 
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote(['top_ask']['price'] (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self): #test2
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
